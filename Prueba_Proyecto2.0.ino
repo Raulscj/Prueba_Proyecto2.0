66e0724c069a4b76ec6bb1f9588cb32b5a3cc9a9
Raulscj <radscjggdiosteama@gmail.com>
2023-11-19 10:58:49 -0400

    server received

--- Prueba_Proyecto2.0.ino
+++ Prueba_Proyecto2.0.ino
@@ -2,9 +2,9 @@
 #include <ThingSpeak.h>
 #include <max6675.h>
 #include <WebServer.h>
-#include <Wire.h>
+/*#include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_SSD1306.h>*/
 #include "config.h"
 #include "data.h"
 
@@ -16,32 +16,33 @@ void setup()
   pinMode(15, OUTPUT);      // Motor
 
   Serial.begin(115200);
-  Wire.begin();
-  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  /*Wire.begin();
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);*/
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.println("Conectando...");
-    oled.clearDisplay();
+    /*oled.clearDisplay();
     oled.setTextColor(WHITE);
     oled.setCursor(0, 0);
     oled.setTextSize(1);
     oled.print("Conectando...");
-    oled.display();
+    oled.display();*/
   }
   Serial.println("Conexión exitosa");
   ThingSpeak.begin(client);
   Serial.println(WiFi.localIP());
-  oled.clearDisplay();
+  /*oled.clearDisplay();
   oled.setTextColor(WHITE);
   oled.setCursor(0, 0);
   oled.setTextSize(1);
   oled.print(WiFi.localIP());
-  oled.display();
+  oled.display();*/
   delay(1000);
 
-  server.on("/", HTTP_GET,[]() {
+  server.on("/", HTTP_GET, []()
+            {
     String html = "<html><body>";
     html += "<h1>Tostadora de Maíz</h1>";
     html += "<p>Tiempo Total: " + String(tiempoTotal) + " segundos</p>";
@@ -68,28 +69,32 @@ void loop()
   server.handleClient();
   if (digitalRead(2) == HIGH && tiempoRestante > 0)
   {
+    
     Serial.println("SYSTEMS ON");
     getTemp();
-    Serial.println('Temperatura:');
-    oled.clearDisplay();
+    Serial.println("Temperatura:");
+    /*oled.clearDisplay();
     oled.setTextColor(WHITE);
     oled.setCursor(0, 0);
     oled.setTextSize(1);
     oled.print('Temperatura: ');
-    oled.setCursor(36,0);
+    oled.setCursor(36, 0);
     oled.setTextSize(1);
-    oled.print(tempC);
+    oled.print(tempC);*/
     Serial.println(tempC);
-    Serial.println('Tiempo');
-    oled.setCursor(0, 30);
+    Serial.println("Tiempo ");
+    Serial.println(tiempoRestante);
+    /*oled.setCursor(0, 30);
     oled.setTextSize(1);
     oled.print('Tiempo restante: ');
     oled.setCursor(42, 30);
     oled.setTextSize(1);
     oled.print(tiempoRestante);
-    oled.display();
+    oled.display();*/
     ThingSpeak.setField(3, tiempoRestante);
     ThingSpeak.writeFields(channelID, writeAPIKey);
+    tiempoRestante --;
+    server.handleClient();
   }
   else
   {
@@ -100,8 +105,9 @@ void loop()
 void getTemp()
 {
   tempC = thermocouple.readCelsius();
-  ThingSpeak.setField (1,tempC);
+  ThingSpeak.setField(1, tempC);
 }
-void alarm(){
-  digitalWrite(5,HIGH);
+void alarm()
+{
+  digitalWrite(5, HIGH);
 }
